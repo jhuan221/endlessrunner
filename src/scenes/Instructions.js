9207357f69057a8451929a0d101d4751df44b3e8
Jessica <jhuan221@ucsc.edu>
2022-04-29 18:00:11 -0700

    added how to play

--- src/scenes/Instructions.js
+++ src/scenes/Instructions.js
@@ -3,18 +3,28 @@ class Instructions extends Phaser.Scene{
         super("instructionsScene");
     }
 
+    preload(){
+        this.load.image('howtoplay', './assets/howtoplay.png');
+    }
+
     create(){
         //define key 
         keyRt = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+        this.add.sprite(640,400, 'howtoplay').setOrigin(0.5);
         
         //temp text
-        this.add.text(20,20, ' how to play\n\n\n press → to go back');
+        //this.add.text(20,20, ' how to play\n\n\n press → to go back');
     }
 
     update(){
         // checking if right key is pressed to go back to menu
         if (Phaser.Input.Keyboard.JustDown(keyRt)){
             this.scene.start('menuScene');
+        }
+        if (Phaser.Input.Keyboard.JustDown(keySPACE)){
+            this.scene.start('playScene');
         }
     }
 }
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,20 +9,21 @@ class Menu extends Phaser.Scene {
     }
 
     create(){
+        this.add.sprite(640,400, 'menu').setOrigin(0.5);
         //this.scene.start("playScene");
         // just a temp config for the menu for now
-        let menuConfig = {
-            fontFamily: 'Helvetica',
-            fontSize: '28px',
-            backgroundColor: '#FFFFFF',
-            color: '#0047ab',
-            align: 'right',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-            fixedWidth: 0
-        }
+        // let menuConfig = {
+        //     fontFamily: 'Helvetica',
+        //     fontSize: '28px',
+        //     backgroundColor: '#FFFFFF',
+        //     color: '#0047ab',
+        //     align: 'right',
+        //     padding: {
+        //         top: 5,
+        //         bottom: 5,
+        //     },
+        //     fixedWidth: 0
+        // }
 
         // temporary menu text
         // this.add.text(640, 400, 'Endless Strummer', menuConfig).setOrigin(0.5);
@@ -34,15 +35,14 @@ class Menu extends Phaser.Scene {
         keyLt = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
 
         // animation config
-        this.anims.create({
-            key: 'show', 
-            frames: this.anims.generateFrameNumbers('title', {start: 0, end: 149, first: 0}),
-            frameRate: 40
-        });
+        // this.anims.create({
+        //     key: 'show', 
+        //     frames: this.anims.generateFrameNumbers('title', {start: 0, end: 149, first: 0}),
+        //     frameRate: 40
+        // });
     }
 
     update(){
-        this.add.sprite(640,400, 'menu').setOrigin(0.5);
         if (Phaser.Input.Keyboard.JustDown(keySPACE)){
             //console.log("space is pressed");
             this.scene.start('playScene');
